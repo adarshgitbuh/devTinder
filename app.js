f3ab7fbb4cf66cbd542bc8eb0e89ad6f5775bec2
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-02-26 15:11:28 +0530

    playing with routes

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
-
-
-app.get("/user/:userId/:name/:password", (req,res) => {
-    console.log(req.params);
-    res.send({firstname : "adarsh", lastname: "shetty"});
-});
-
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the server");
+    next();
+    // res.send("response1");
+  },
+  (req, res,next) => {
+    //res.send("response2");
+    console.log("Handling the req"q);
+    next();
+  },
+  (req, res) => {
+    res.send("response3");
+    console.log("handler...3");
+  },
+  (req, res) => {
+    res.send("respon44");
+    console.log("hendler55");
+  }
+);
 
 app.listen(3000, () => {
-    console.log("server is running on the port 3000..")
+  console.log("server is running on the port 3000..");
 });
