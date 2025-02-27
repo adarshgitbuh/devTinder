0295763a20bb471a64c7b73b6b836a234e213878
Adarsh Shetty <adarsh.int@niveussolutions.com>
2025-02-27 11:12:56 +0530

    "added erroer handlers"

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 const express = require("express");
 
 const app = express();
+const { adminAuth, userAuth } = require("./midlewares/auth");
+//Handles Auth middlewares for all GET, POST -> all HTTP req..
+app.use("/admin", adminAuth);
 
-app.get(
-  "/user",
-  (req, res, next) => {
-    console.log("handling the server");
-    next();
-    // res.send("response1");
-  },
-  (req, res,next) => {
-    //res.send("response2");
-    console.log("Handling the req"q);
-    next();
-  },
-  (req, res) => {
-    res.send("response3");
-    console.log("handler...3");
-  },
-  (req, res) => {
-    res.send("respon44");
-    console.log("hendler55");
+app.get("/user",userAuth, (req,res) => {
+  res.send("user data sent");
+});
+
+app.get("/admin/getAllData", (req, res) => {
+  res.send("User data sent");
+});
+
+app.use("/",(err,req,res,next) => {
+  if(err) {
+    res.status(500).send("somethig went wrong")
   }
-);
+})
+  
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("all users deleted");
+});
 
 app.listen(3000, () => {
   console.log("server is running on the port 3000..");
